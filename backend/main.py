c8eda2df12a2d2bacd5c16885cf79063d176b662
unknown <margarita.piscenco@tum.de>
2023-11-18 17:35:43 +0100

    added backend api

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import openai
-import asyncio
-from openai import AsyncOpenAI, OpenAI
-from openai.types.chat import ChatCompletion
+from openai import OpenAI
 import time
 import re
+import random
+from PIL import Image
+import os
 
 recipes_list_pattern = '''Provide names and ingredients of food recipes based on the users description 
 Names of 5 recipes in the following format recipe name # food type (one of the following: Pizza, Hamburger, Pasta, Cake)
@@ -22,6 +18,13 @@ Recipe name: {}
 Output: a numerated list of and ingredients 
 and enumerated cooking steps without an introduction.'''
 
+recipes_list_search_pattern = '''Provide names and ingredients of food recipes based on the users description 
+Names of 5 recipes in the following format recipe name # food type (one of the following: Pizza, Hamburger, Pasta, Cake)
+Examples:
+case: Given user likes italian food, is not vegan Recommend: Salami Pizza 
+case: Given user likes spicy food, is vegan Recommend: spicy salad with tabasco sauce
+case: User is likes sweets and is allergic to chocolate Recommend: 
+'''
 global_api_key = ''
 rec_list_pattern = r'[0-9]+\. (.*) # (.*)'
 
@@ -42,7 +45,7 @@ def ask_gpt_recipe_list(client):
     return re.findall(rec_list_pattern, chat_recom_text.choices[0].message.content)
 
 
-def ask_gpt_recipe_desc(client, recipe_name, food_type=None):
+def ask_gpt_recipe_desc(client, recipe_name):
 
     start_time = time.time()
     chat_recom_text = client.chat.completions.create(
@@ -55,13 +58,35 @@ def ask_gpt_recipe_desc(client, recipe_name, food_type=None):
         model="gpt-3.5-turbo-1106",# gpt-3.5-turbo
     )
     print("--- %s seconds ---" % (time.time() - start_time))
-    return chat_recom_text.choices[0].message.content# re.findall(rec_list_pattern, chat_recom_text.choices[0].message.content)
+    return chat_recom_text.choices[0].message.content
 
+def read_images():
+    # Path to the folder containing images
+    folder_path = "C:\\random_projects\\remy\\backend\\img_db"
+    # Dictionary to store images, because its an mvp
+    images_dict = {}
+    print(os.listdir(folder_path))
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            name_without_extension = os.path.splitext(filename)[0]  # Extracting name without extension
+            image_path = os.path.join(folder_path, filename)
+            img = Image.open(image_path)
+            images_dict[name_without_extension] = img
+    # Now image_dict contains images with filenames (without extensions) as keys
+    return images_dict
+
+
+def get_image(class_type):
+    image_key = "{}_{}".format(class_type, str(random.randint(1, 5)))
+    return image_db[image_key]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # import keys from the file
+    image_db = read_images()
+    our_img = get_image('pizza')
+    our_img.show()
+    '''# import keys from the file
     f = open("keys.txt", "r")
     lines = f.readlines()
     global_api_key = lines[0]
@@ -71,5 +96,5 @@ if __name__ == '__main__':
     recommendation_list = ask_gpt_recipe_list(sync_client)
     print(recommendation_list)
     some_dish_name = 'Chocolate cake'
-    recom_descr = ask_gpt_recipe_desc(sync_client, recommendation_list[0][0], recommendation_list[0][1])
-    print(recom_descr)
+    recom_descr = ask_gpt_recipe_desc(sync_client, recommendation_list[0][0])
+    print(recom_descr)'''
