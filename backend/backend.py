fb3b8b77f2bca5a97b4712e95dd7bfa21a922135
unknown <margarita.piscenco@tum.de>
2023-11-19 04:52:16 +0100

    added constructing user pattern

--- backend/backend.py
+++ backend/backend.py
@@ -11,7 +11,6 @@ import os
 import httpx
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 PEXELS_API_KEY = os.environ.get("PEXELS_API_KEY")
@@ -26,13 +25,15 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
 class Rating(BaseModel):
     recipeId: int
     rating: int
 
 
 pref_user = ""
-
+initial_constr_user = ''
+user_summary = ''
 
 recipes_list_pattern = '''
 Names of 5 recipes in the following format: recipe name
@@ -65,8 +66,10 @@ sync_client = OpenAI(api_key=global_api_key)
 # here could be a database
 the_database = dict()
 
+
 def generate_user_id():
     return random.randint(1000, 9999)
+
 
 def ask_gpt_recipe_list(client, user_descr):
     start_time = time.time()
@@ -84,7 +87,14 @@ def ask_gpt_recipe_list(client, user_descr):
     return re.findall(rec_list_pattern, chat_recom_text.choices[0].message.content)
 
 
-def ask_gpt_recipe_desc(client, recipe_name, user_descr):
+def ask_gpt_recipe_desc(client, recipe_name):
+    # aggregate user descr from musts and preferences
+    user_descr = initial_constr_user
+    if user_summary != '':
+        print('user has some rated dishes')
+        if user_descr != '':
+            user_descr += ','
+        user_descr += user_summary
     start_time = time.time()
     chat_recom_text = client.chat.completions.create(
         messages=[
@@ -98,6 +108,23 @@ def ask_gpt_recipe_desc(client, recipe_name, user_descr):
     print("--- %s seconds ---" % (time.time() - start_time))
     return chat_recom_text.choices[0].message.content
 
+def ask_gpt_to_summarize_user_pref(client, ratings_dict):
+    pref = ', '.join([f'{key} {value}' for key, value in ratings_dict.items()])
+    summary_message = summarize_pattern.format(pref)
+    start_time = time.time()
+    chat_recom_text = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": summary_message
+            }
+        ],
+        model="gpt-3.5-turbo-1106",  # gpt-3.5-turbo
+    )
+    print("--- %s seconds ---" % (time.time() - start_time))
+    # Extract dishes names
+    print(chat_recom_text.choices[0].message.content)
+    return re.findall(rec_list_pattern, chat_recom_text.choices[0].message.content)
 
 def read_images():
     # Path to the folder containing images
@@ -120,43 +147,48 @@ def read_images():
 def get_recommendation(item_id: int, q: str = None):
     return {'some salutation': 'Hii'}
 
+
 @app.post("/submit-ratings")
 async def submit_ratings(ratings: Dict[str, int]):
     try:
         # Process the ratings here
         # For example, store them in a database
         pref_user = ', '.join([f'{key} {value}' for key, value in ratings.items()])
-
+        user_summary = ask_gpt_to_summarize_user_pref(sync_client, ratings)
+        print('user summary generated')
         print(ratings)
         return {"message": "Ratings submitted successfully"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@app.post("/new_user}")
-async def create_user(user_pref_data: dict):
+
+@app.post("/user_pref_data}")
+async def post_restrictions(user_pref_data: list):
     # Process the received user_data (it's a dictionary)
     # save it to a database and perform other operations
     # Generate a random user_id
-    user_id = generate_user_id()
     # save to database user's preferences
     # likes chicken, has allergies tomatoes, omnivore
     '''user_descr = "likes {}, has allergies {}, {}".format(", ".join(user_pref_data['likes']),
                                                          ", ".join(user_pref_data['allergies']),
                                                          user_pref_data['preference'])    '''
-    user_descr = f"likes {', '.join(user_pref_data['likes'])}, has allergies {', '.join(user_pref_data['allergies'])}, {user_pref_data['preference']}"
-    the_database[user_id] = user_descr
+    initial_constr_user = "User has allergies {}".format(", ".join(user_pref_data))
+    print('saved dietory restrictions. post_restrictions func call')
+
+
+    # user_descr = f"likes {', '.join(user_pref_data['likes'])}, has allergies {', '.join(user_pref_data['allergies'])}, {user_pref_data['preference']}"
+    # the_database[user_id] = user_descr
 
 
 @app.get("/recommendations/{user_id}")
 def get_recommendation(user_id: int, q: str = None):
-    recommendation_list = ask_gpt_recipe_list(sync_client, user_id) # if the function needs it
+    recommendation_list = ask_gpt_recipe_list(sync_client, user_id)  # if the function needs it
     return recommendation_list
-
 
 
 @app.get("/recommendations/recipe_desc/{recipe_name}")
 def get_desription(recipe_name: str):
-    recom_descr = ask_gpt_recipe_desc(sync_client, recipe_name, "likes chicken")
+    recom_descr = ask_gpt_recipe_desc(sync_client, recipe_name)
     print(recom_descr)
     return {"dish name": recipe_name, "recipe": recom_descr}
 
